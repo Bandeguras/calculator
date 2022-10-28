a42440b2b5b5992fe1d109f7aef27f39f0a2891e
Said Saliev <repsaid@gmail.com>
2022-10-28 15:34:10 +0600

    configured the view and urls

--- source/calculator/urls.py
+++ source/calculator/urls.py
@@ -15,7 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-
+from webapp.views import calculator_view
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', calculator_view),
 ]
--- source/webapp/views.py
+++ source/webapp/views.py
@@ -1,3 +1,32 @@
 from django.shortcuts import render
 
-# Create your views here.
+from django.shortcuts import render
+
+
+def calculator_view(request):
+    if request.method == "GET":
+        return render(request, "calculator.html")
+
+    elif request.method == "POST":
+        context = {
+            'first': request.POST.get('first'),
+            'second': request.POST.get('second'),
+            'method': request.POST.get('method'),
+            'result': 0,
+        }
+        context['first'] = int(context['first'])
+        context['second'] = int(context['second'])
+
+        if context['method'] == '+':
+            context['result'] += (context['first'] + context['second'])
+
+        elif context['method'] == '-':
+            context['result'] += (context['first'] - context['second'])
+
+        elif context['method'] == '*':
+            context['result'] += (context['first'] * context['second'])
+
+        elif context['method'] == '/':
+            context['result'] += (context['first'] / context['second'])
+
+        return render(request, 'calculator.html', context)
